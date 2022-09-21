7c483ef82624576927c914bab7a9169045f3063f
IG <iikim2511@gmail.com>
2022-09-21 16:48:45 -0400

    post

--- server.js
+++ server.js
@@ -3,18 +3,41 @@ const app = express();
 
 var colors = require("colors");
 
+
+// 🍀get, post, put, delete
+
+// 🍀get
 app.get("/", function (req, res) {
   //res.send('ig node server')
   res.sendFile(__dirname + "/index.html");
 });
+
 app.get("/style.css", function (req, res) {
   res.sendFile(__dirname + "/style.css");
 });
 
-app.get("/pet", function (req, res) {
-  res.send("hi pet");
-});
+app.get("/write", function (req, res) {
+    //res.send('ig node server')
+    res.sendFile(__dirname + "/write.html");
+  });
 
-app.listen(8080, function () {
+
+// 🍀post
+app.post('/add',function (req,res) {    
+    res.send('/add, 전송완료')
+})
+
+
+
+
+
+
+
+
+
+
+
+// 🍀listen
+app.listen(3000, function () {
   console.log("ig node server gogo".rainbow);
 });
