0dc3812f0c80a8ef8f2801fd16680875e9a0b88e
IG <iikim2511@gmail.com>
2022-09-21 23:09:17 -0400

    db.collection('co0921').insertOne(

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ let colors = require("colors");
 let bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 
+require('dotenv').config()
+
 let MongoClient = require('mongodb').MongoClient;
 
-require('dotenv').config()
 
 
 // 🍀get, post, put, delete
@@ -29,13 +30,7 @@ app.get("/write", function (req, res) {
   });
 
 
-// 🍀post, bodyParser
-app.post('/add',function (req,res) {    
-    res.send('/add, 전송완료')
 
-    console.log(req.body)
-    console.log(req.body.ig_title)
-})
 
 
 // 🦄🦄c28 
@@ -46,15 +41,38 @@ MongoClient.connect(url, function(err, client) {
   if (err) throw err;
   console.log("ig-Database created!");
 
+
   
-// 🍀listen
-app.listen(process.env.PORT, function () {
-    console.log("ig node server gogo".rainbow);
+  let db = client.db('db0921');
+  db.collection('co0921').insertOne({title:'hi',data:1, name:'john,'},function (err, p_res) {
+    console.log('insertone success')
+    
+  })  
+
+
+
+  // 🍀post, bodyParser
+  app.post('/add',function (req,res) {    
+    res.send('/add, 전송완료')
+
+    console.log(req.body)
+    console.log(req.body.ig_title)
+
+    db.collection('co0921').insertOne({title: req.body.ig_title, date:req.body.ig_data },function (){
+      console.log('insertone success'.blue)
+      
+    })
+  })
+  
+
+  // 🍀listen
+  app.listen(process.env.PORT, function () {
+      console.log(`ig node server gogo, port: ${process.env.PORT}`.rainbow);
   });
-  
 
 
-  client.close();
+  // cliend.close()있으면 post가 안됨..왜인지는 모름
+  // client.close();
 });
 
 
